3ee5b5c546ca6f205d313e21ac07e79e02321e90
mayazaki98 <n.yamamoto98@gmail.com>
2024-04-06 02:44:48 +0900

    ♻️ Main部分をコンポーネント化

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,6 +1,3 @@
-import { Footer } from "@/components/Footer";
-import Image from "next/image";
-
 export function Links() {
   return (
     <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
